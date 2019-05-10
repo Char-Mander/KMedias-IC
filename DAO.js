546732f7b24446c442eee01b76e06f14b006cbd5
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2019-05-10 14:24:41 +0200

    Añadido el icono a la pestaña

--- DAO.js
+++ DAO.js
@@ -6,21 +6,12 @@ class DAO {
     }
     /*Devuelve todos los datos del usuario cuyo email es el introducido*/
     getData(callback) {
-        /*
-                 connection.query(sql, [id], function(err, resultado){
-                          connection.release();¡
-                if (err) {
-                    callback(new Error("Error de acceso a la base de datos"), null);
-                } else {
-                    callback(null, data[0]);
-                }
-        
-                 });*/
         fs.readFile(this.file, (err, data) => {
             if (err) {
                 callback(new Error("Error al leer el fichero")), null;
             }
             else {
+                console.log(Array.from(data.toString()));
                 callback(null, data.toString());
             }
         })
